666ae6859020f7c614a82a865e9a5deac94a4394
Your Name <br.godoys@duocuc.cl>
2025-11-10 04:01:27 -0300

    cambio pequeño de estilo en inputs bloqueados

--- src/pages/store/PrivacidadPage.js
+++ src/pages/store/PrivacidadPage.js
@@ -124,7 +124,7 @@ const PrivacidadPage = () => {
                                 <Form.Control
                                     type="email"
                                     value={currentUser ? currentUser.email : ''}
-                                    readOnly // <-- CAMBIO DE 'disabled' A 'readOnly'
+                                    disabled
                                 />
                             </Form.Group>
 
