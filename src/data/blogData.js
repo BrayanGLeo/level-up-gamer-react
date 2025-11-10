834fa823ee4a4d6c9250a37a8c21337f58351ec7
Dani14789 <dan.godoys@duocuc.cl>
2025-11-10 04:44:21 -0300

    Adicion de nuevos blogs

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,69 +1,103 @@
 export const blogPosts = [
+
     {
         id: 1,
+        title: 'Los dueños de FromSoftware están encantados con Elden Ring Nightreign, tanto que hasta ya están preparando un DLC',
+        date: '9 Noviembre 2025',
+        image: 'https://i.blogs.es/960634/original/1200_800.jpeg',
+        content: 'Los dueños de FromSoftware confirman que Elden Ring Nightreign ha rendido por encima de lo previsto y sitúa su primer DLC dentro del año fiscal 2025.'
+    },
+
+    {
+        id: 2,
+        title: 'Es oficialmente la mayor dinastía en los 15 años de League of Legends. Los Worlds 2025 acaban con una victoria histórica de T1',
+        date: '9 Noviembre 2025',
+        image: 'https://i.blogs.es/cd733e/t1-lol-worlds-2025/1200_800.jpeg',
+        content: 'El equipo liderado por Faker ha conseguido levantar su tercer campeonato consecutivo y el sexto de su historia.'
+    },
+
+    {
+        id: 3,
+        title: 'Salió hace una semana y ya es el multijugador mejor valorado en casi 10 años. Lo de ARC Raiders es alucinante',
+        date: '7 Noviembre 2025',
+        image: 'https://i.blogs.es/0956c8/arc-raiders/1200_800.jpeg',
+        content: 'En cuanto a los shooter multijugador exclusivos, sin modo campaña o similares, el juego de Embark Studios ha alcanzado un hito.'
+    },
+
+    {
+        id: 4,
+        title: '¡GTA 6 se retrasa otra vez!',
+        date: '6 Noviembre 2025',
+        image: 'https://i.blogs.es/ae9cd2/original/1200_800.jpeg',
+        content: 'Rockstar pide disculpas y anuncia que su juego de mundo abierto se hará de rogar varios meses más.'
+    },
+
+
+    {
+        id: 5,
         title: 'PlayStation anuncia los 8 juegos gratis de septiembre de 2025 para miembros de PS Plus Extra y Premium',
         date: '10 Septiembre 2025',
         image: '/img/Play plus.jpg',
         content: 'El Catálogo de PS Plus Extra y Premium se actualiza el 16 de septiembre con una variedad de géneros: lucha libre, estrategia y supervivencia.'
     },
     {
-        id: 2,
+        id: 6,
         title: '¡Madrid y Barcelona dominan! EA SPORTS FC 26 anuncia los mejores jugadores de LALIGA y Liga F, y hay sorpresas',
         date: '10 Septiembre 2025',
         image: '/img/FC26.jpg',
         content: 'La Semana de las Valoraciones de EA SPORTS FC 26 pone el foco en estrellas como Mbappé, Lamine Yamal, Aitana Bonmatí y Alexia Putellas.'
     },
     {
-        id: 3,
+        id: 7,
         title: 'NVIDIA RTX Remix recibe un sistema avanzado de partículas con trazado de rayos',
         date: '09 Septiembre de 2025',
         image: '/img/RTXs.jpg',
         content: 'NVIDIA ha anunciado una nueva actualización de RTX Remix, su plataforma de modding para PC con GPU GeForce RTX. La principal novedad es la incorporación de un sistema de partículas con path tracing que permite mejorar efectos clásicos como fuego y humo, además de añadir partículas con físicas realistas y reflejos dinámicos.'
     },
     {
-        id: 4,
+        id: 8,
         title: 'Jugadores temen que la próxima consola de Xbox sea demasiado cara por aumentos recientes en el precio de Series X|S',
         date: '20 de Septiembre de 2025',
         image: 'https://i0.wp.com/levelup.buscafs.com/2025/09/Precio-Xbox.jpg?fit=1225,610&quality=75&strip=all',
         content: 'Microsoft subió el precio de Xbox Series X|S una vez más por culpa de los aranceles y demás factores económicos. De esta forma, el Xbox Series S costará $399 USD y el Xbox Series X de 2TB alcanzará el elevado costo de $799 USD en Estados Unidos. Los jugadores consideran que las cosas se están saliendo de control, pues nunca imaginaron que los sistemas aumentarían tanto de precio en tan poco tiempo.'
     },
     {
-        id: 5,
+        id: 9,
         title: 'Hay un nuevo shooter táctico gratis petándolo en Steam con un notable y casi 60.000 jugadores',
         date: '22 de Septiembre de 2025',
         image: 'https://i.blogs.es/0683a3/maxresdefault-1-_upscayl_2x_remacri-4x-1-/1200_800.jpeg',
         content: 'Arena Breakout: Infinite. El título de Morefun Studios, que combina acción táctica y mecánicas de extracción, se ha convertido en una de las sorpresas más destacadas de septiembre, atrayendo a decenas de miles de jugadores en muy poco tiempo.'
     },
     {
-        id: 6,
+        id: 10,
         title: 'PlayStation 5: Potencia de Nueva Generación',
         date: '12 de Octubre de 2024',
         image: '/img/Play 5.jpg',
         content: 'La PS5 no es solo una consola, es un centro de entretenimiento inmersivo. Con su SSD de ultra alta velocidad, los tiempos de carga son cosa del pasado. El control DualSense con su respuesta háptica y gatillos adaptativos te hace sentir cada acción del juego. Una experiencia revolucionaria.'
     },
     {
-        id: 7,
+        id: 11,
         title: 'Mando Inalámbrico Xbox: Precisión y Comodidad',
         date: '8 de Octubre de 2024',
         image: '/img/Mando.jpg',
         content: 'Microsoft ha perfeccionado un diseño ya legendario. El mando de Xbox Series X mejora el agarre con superficies texturizadas, una nueva cruceta híbrida para movimientos más precisos y el botón "Compartir" para capturar tus mejores momentos al instante. Es el compañero ideal para cualquier PC o consola Xbox.'
     },
     {
-        id: 8,
+        id: 12,
         title: 'HyperX Cloud II: Escucha Cada Detalle',
         date: '3 de Octubre de 2024',
         image: '/img/Audifonos.jpg',
         content: 'Un clásico por una razón. Los HyperX Cloud II son famosos por su increíble comodidad, permitiendo horas de juego sin fatiga. Su calidad de sonido envolvente 7.1 te da una ventaja competitiva al escuchar cada paso enemigo, y su micrófono desmontable con cancelación de ruido asegura una comunicación cristalina.'
     },
     {
-        id: 9,
+        id: 13,
         title: 'Logitech G502 HERO: El Rey de la Precisión',
         date: '25 de Septiembre de 2024',
         image: '/img/Mause.jpg',
         content: 'El G502 HERO es un ícono en el mundo del gaming. Equipado con el sensor HERO 25K para una precisión inigualable, cuenta con 11 botones programables y un sistema de pesas ajustables para que lo adaptes a tu estilo de juego. Su diseño ergonómico lo hace perfecto tanto para jugar como para trabajar.'
     },
     {
-        id: 10,
+        id: 14,
         title: 'Secretlab TITAN: El Trono Definitivo del Gamer',
         date: '18 de Septiembre de 2024',
         image: '/img/Silla.jpg',
